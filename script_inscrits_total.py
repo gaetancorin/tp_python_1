506254585e9d7f83ba132ff65c0aa7372a1b549e
gaetancorin <gaetancorin@gmail.com>
2022-01-11 14:52:02 +0100

    maj script_inscrits_total ne pas ajouter les doublons

--- script_inscrits_total.py
+++ script_inscrits_total.py
@@ -17,15 +17,27 @@ chemin = os.getcwd()
 liste = (os.listdir(chemin))
 print(liste)
 
-#trouver dans liste les fichiers commencant par inscrit-20, ouvrir et pour chaque ligne, ecrire dans inscrit-total
+#trouver dans liste les fichiers commencant par inscrit-20
 for i in liste:
     if re.match("inscrits-20",i):
         print(i)
+#ouvrir chaque fichier qui match et afficher chaque ligne
         with open(i,"r") as dossier:
-            spamreader = csv.reader(dossier, delimiter=";")
-            for line in spamreader:
+            fichierliste = csv.reader(dossier, delimiter=";")
+            for line in fichierliste:
                 print(line)
-                with open("inscrits_total/inscrits_total.csv", 'a') as csvfile:
-                    spamwriter = csv.writer(csvfile, delimiter=';')
-                    spamwriter.writerow([line[0], line[1], line[2], line[3]])
+# ouvrir fichier inscrit_total, afficher chaque ligne et comparer avec la ligne du fichier qui match /  si pareil, doublon =1
+                doublon = 0
+                csvfile = open("inscrits_total/inscrits_total.csv", "r")
+                spamreader = csv.reader(csvfile, delimiter=";")
+                for tuple_total in spamreader:
+                    if tuple_total == line:
+                        print("doublon-refuser")
+                        doublon = 1
+                csvfile.close()
+# Si aucune ligne identique entre la totalité de inscrit_total et la ligne du fichier qui match, ajouter la ligne a inscrits-total
+                if doublon != 1:
+                    with open("inscrits_total/inscrits_total.csv", 'a') as csvfile:
+                        spamwriter = csv.writer(csvfile, delimiter=';')
+                        spamwriter.writerow([line[0], line[1], line[2], line[3]])
 
