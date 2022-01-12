57d458d35c3a79b00b890b18607c450cf0779f6f
gaetancorin <gaetancorin@gmail.com>
2022-01-12 12:29:06 +0100

    fin de liste decroissante des inscrit par categorie

--- script_inscrits_total.py
+++ script_inscrits_total.py
@@ -8,21 +8,21 @@ except FileExistsError:
     pass
 
 
-#reinitialise inscrits-total
+# reinitialise inscrits-total
 f = open("inscrits_total/inscrits_total.csv", 'w')
 f.close()
 
-#trouver tous les fichiers voisins et les mets dans une liste
+# trouver tous les fichiers voisins et les mets dans une liste
 chemin = os.getcwd()
 liste = (os.listdir(chemin))
 print(liste)
 
-#trouver dans liste les fichiers commencant par inscrit-20
+# trouver dans liste les fichiers commencant par inscrit-20
 for i in liste:
-    if re.match("inscrits-20",i):
+    if re.match("inscrits-20", i):
         print(i)
-#ouvrir chaque fichier qui match et afficher chaque ligne
-        with open(i,"r") as dossier:
+# ouvrir chaque fichier qui match et afficher chaque ligne
+        with open(i, "r") as dossier:
             fichierliste = csv.reader(dossier, delimiter=";")
             for line in fichierliste:
                 print(line)
@@ -41,24 +41,34 @@ for i in liste:
                         spamwriter = csv.writer(csvfile, delimiter=';')
                         spamwriter.writerow(line)
 
-Non_admis = 0
-Poussin = 0
-Cadet = 0
-Junior = 0
-Semi_pro = 0
-Pro = 0
 
-# with open("inscrits_total/inscrits_total.csv", "r") as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=";")
-#     for line in csvfile:
-#         print(line[2])
+categorie = [["Non_admis"], ["Poussin"], ["Cadet"], ["Junior"], ["Semi_pro"], ["Pro"]]
 
+# Recuperer la categorie de chaque ligne de inscrits-total(csv)
 with open("inscrits_total/inscrits_total.csv", "r", newline='') as f:
     fieldnames = ['nom', 'prenom', 'email', 'categorie']
     reader = csv.DictReader(f, fieldnames=fieldnames, delimiter=';')
     for line in reader:
-        print(line['categorie'])
+        # print(line['categorie'])
+# comparer le resultat de categorie(du csv) avec la liste categorie, ajouter si egal
+        for categ in categorie:
+            if categ[0] == line['categorie']:
+                categ.append(line)
+            else:
+                pass
+
+# range le tableau categorie du plus remplis en moins remplis
+categorie.sort(key=len, reverse=True)
+
+# print les enregistrement classé par cotegorie de manière decroissante
+for categor in categorie:
+    print("---------")
+    for enregistrement in categor:
+        print(enregistrement)
 
 
-
-
+# with open("inscrits_total/inscrits_total.csv", "r", newline='') as f:
+#     fieldnames = ['nom', 'prenom', 'email', 'categorie']
+#     reader = csv.DictReader(f, fieldnames=fieldnames, delimiter=';')
+#     for line in reader:
+#         print(line['categorie'])
